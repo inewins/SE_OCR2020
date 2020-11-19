1692303b8615ce02d4606c3169bcb9e5ebc77302
Anh <anh.nguyen2@mavs.uta.edu>
2020-11-18 21:21:40 -0600

    update so far

--- app/src/main/java/com/example/nutrobud/Settings_Main.java
+++ app/src/main/java/com/example/nutrobud/Settings_Main.java
@@ -1,5 +1,6 @@
 package com.example.nutrobud;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,11 +11,22 @@ import android.widget.Button;
 
 import com.example.nutrobud.ui.home.Stats;
 import com.example.nutrobud.ui.home.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Settings_Main extends AppCompatActivity {
+
+    private DocumentReference userDB = FirebaseFirestore.getInstance().collection("users").document("10006");//Firestore ref to pull user data
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,21 +51,34 @@ public class Settings_Main extends AppCompatActivity {
         ingredientsYesTrackedQty.add("2");
         ingredientsYesTrackedQty.add("3");
 
-        User User = new User();
-        User.setId(10002);
-        User.setEmail("anh.nguyen2@mav.uta.edu");
-        User.setPassword("myPassWo@rd");
-        User.setFirstName("Anh");
-        User.setSecondName("Nguyen");
-        User.setGender("m");
-        User.setAge(69);
-        User.setWeight(169);
-        User.setIngredientsNo(ingredientsNo);
-        User.setIngredientsYes(ingredientsYes);
-        User.setIngredientsYesGoalsQty(ingredientsYesGoalsQty);
-        User.setIngredientsYesTrackedQty(ingredientsYesTrackedQty);
-        User.setCalorieGoalsQty(2000);
-        User.setcalorieTrackedQty(2000);
+        final User[] user = {new User()};
+        user[0].setId(10002);
+        user[0].setEmail("anh.nguyen2@mav.uta.edu");
+        user[0].setPassword("myPassWo@rd");
+        user[0].setFirstName("Anh");
+        user[0].setSecondName("Nguyen");
+        user[0].setGender("m");
+        user[0].setAge(69);
+        user[0].setWeight(169);
+        user[0].setIngredientsNo(ingredientsNo);
+        user[0].setIngredientsYes(ingredientsYes);
+        user[0].setIngredientsYesGoalsQty(ingredientsYesGoalsQty);
+        user[0].setIngredientsYesTrackedQty(ingredientsYesTrackedQty);
+        user[0].setCalorieGoalsQty(2000);
+        user[0].setcalorieTrackedQty(2000);
+
+        userDB.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()){
+                    DocumentSnapshot doc = task.getResult();
+                    if(doc.exists()){
+                        Log.d("User Data",doc.getData().toString());
+                    }
+                }
+            }
+        });
+
         /*End of Creating User*/
 
         Button btn2EditProfile = (Button)findViewById(R.id.editProfileBtn);                                     //Button to go to EditProfile page
