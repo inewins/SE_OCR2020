1fb4355fb78c69cf2cbc104cf69268486f75cf75
WorkHarder25 <lsarver14@gmail.com>
2020-11-24 16:21:05 -0600

    Update Loning/Sign up 11.24.20

--- app/src/main/java/com/example/nutrobud/Login.java
+++ app/src/main/java/com/example/nutrobud/Login.java
@@ -65,8 +65,8 @@ public class Login extends AppCompatActivity {
         };
 
         //If there is already a user active through authenticator, they will be automatically logged in
-       /* if(fAuth.getCurrentUser() != null)
-            startActivity(new Intent(getApplicationContext(), DashActivity.class));*/
+       if(fAuth.getCurrentUser() != null)
+            startActivity(new Intent(getApplicationContext(), DashActivity.class));
        
 
         loginbtn.setOnClickListener(new View.OnClickListener() {
