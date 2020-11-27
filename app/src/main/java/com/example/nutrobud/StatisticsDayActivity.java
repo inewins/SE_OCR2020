77b9598ba0fcbb7b3c631de59c609657c63dd9ac
michelle <m.duong05@gmail.com>
2020-11-26 23:44:01 -0600

    update day stats but want to add picture

--- app/src/main/java/com/example/nutrobud/StatisticsDayActivity.java
+++ app/src/main/java/com/example/nutrobud/StatisticsDayActivity.java
@@ -16,6 +16,7 @@ import android.os.Handler;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -63,6 +64,8 @@ public class StatisticsDayActivity extends AppCompatActivity {
     Button backButton;
     ListView statsView;
     private List<User> userD = new ArrayList<>();
+    TextView noData;
+    ImageView pic;
 
     // variables to get nurtient and calorie data
     int[] nutrGoal;
@@ -116,8 +119,6 @@ public class StatisticsDayActivity extends AppCompatActivity {
         setContentView(R.layout.activity_statistics_day);
 
 
-
-
         statsView = findViewById(R.id.statsView);
 
         // button to go back to initial statistics page to change view
@@ -141,57 +142,56 @@ public class StatisticsDayActivity extends AppCompatActivity {
                         indexCounter++;
                         userDBData = d.toObject(User.class);
                         userData.add(userDBData);
-                        if(userDBData.getEmail().equalsIgnoreCase(currUser.getEmail())){
+                        if(userDBData.getEmail().equalsIgnoreCase(currUser.getEmail())) {
                             currUserID = userDBData.getId();
                             currUserIndex = indexCounter;
 
-                            //check if data exists
-                           // if (userData.hasChild(todayDate)) {
+                            // check if data exists for current date and calories tracked
+                            if ((userData.get(currUserIndex).getStats().get(todayDate) != null)) {
                                 if (userData.get(currUserIndex).getStats().get(todayDate).getCaloriesTrackedQty() != NULL) {
                                     currentCals = userData.get(currUserIndex).getStats().get(todayDate).getCaloriesTrackedQty();
+                                } else {
+                                    currentCals = 0;
                                 }
-                            //}
+
+                                // get names of ingredients tracked
+                                Set<String> milliName = userData.get(currUserIndex).getStats().get(todayDate).getNutrients().keySet(); // set keys to Set
+                                nutrName = convert(milliName); //convert to string array
+                                System.out.println("Array of string: " + Arrays.toString(nutrName)); // delete later
+
+                                for (int i = 0; i < nutrName.length; i++) {
+                                    counter++;
+                                }
+                                System.out.println("Counter is: " + counter); // delete later
+
+                                // run through each ingredient to get intake amount
+                                for (int i = 0; i < counter; i++) {
+                                    nutrientName = nutrName[i];
+                                    if (userData.get(currUserIndex).getStats().get(todayDate).getNutrients().get(nutrName[i]) != NULL) {
+                                        intake = userData.get(currUserIndex).getStats().get(todayDate).getNutrients().get(nutrName[i]);
+                                    } else {
+                                        intake = 0;
+                                    }
+                                    System.out.println(nutrientName + ": " + intake + " mg");
+
+                                }
+
+                                // make list to display statistics
+                                final ArrayList<String> output = new ArrayList<>();
+                                output.add("calories: " + currentCals + " cals");
+                                for (int i = 0; i < counter; i++) {
+                                    output.add(nutrName[i] + ": " + userData.get(currUserIndex).getStats().get(todayDate).getNutrients().get(nutrName[i]) + " mg");
+                                }
+                                final ArrayAdapter outputAdapted = new ArrayAdapter(StatisticsDayActivity.this, android.R.layout.simple_list_item_1, output);
+                                statsView.setAdapter(outputAdapted);
+                            }
+                            // if data does not exist for current date
                             else {
-                                currentCals = 0;
-                            }
-                            System.out.println("total cals: " + currentCals);
-
-                            // get names of ingredients tracked
-                            Set<String> milliName = userData.get(currUserIndex).getStats().get(todayDate).getNutrients().keySet(); // set keys to Set
-                            nutrName = convert(milliName); //convert to string array
-                            System.out.println("Array of string: " + Arrays.toString(nutrName)); // delete later
-
-                            for (int i=0; i<nutrName.length; i++){
-                                counter++;
-                            }
-                            System.out.println("Counter is: " + counter); // delete later
-
-                            // run through each ingredient to get intake amount
-                            for (int i=0; i<counter; i++){
-                                nutrientName = nutrName[i];
-                                if (userData.get(currUserIndex).getStats().get(todayDate).getNutrients().get(nutrName[i]) != NULL) {
-                                    intake = userData.get(currUserIndex).getStats().get(todayDate).getNutrients().get(nutrName[i]);
-                                }
-                                else{
-                                    intake = 0;
-                                }
-                                System.out.println(nutrientName + ": "  + intake + " mg");
+                                noData=(TextView) findViewById(R.id.noDataDisplay);
+                                noData.setText("No data is available. Please scan nutrition label to view nutritional statistics!");
 
                             }
-
                         }
-                        // make list to display statistics
-                        final ArrayList<String> output = new ArrayList<>();
-                        output.add("calories: " + currentCals + " cals");
-                        for (int i=0; i<counter; i++){
-                            output.add(nutrName[i] + ": " + userData.get(currUserIndex).getStats().get(todayDate).getNutrients().get(nutrName[i]) + " mg");
-                        }
-
-                        final ArrayAdapter outputAdapted = new ArrayAdapter(StatisticsDayActivity.this, android.R.layout.simple_list_item_1, output);
-
-                        statsView.setAdapter(outputAdapted);
-
-
                     }
                     dr = FirebaseFirestore.getInstance().document("users/"+currUserID);//Document ref to post data
                 }
